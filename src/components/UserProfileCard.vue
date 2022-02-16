de865e1da539cb32e823faea0b215eb28c29e12e
beatlin <art851025@gmail.com>
2022-02-16 17:26:44 +0800

    Update UserComponets

---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,101 @@
+<template>
+<div class="card mb-3">
+    <div class="row no-gutters">
+        <div class="col-md-4">
+            <img :src="profile.image" width="300px" height="300px">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">{{profile.name}}</h5>
+                <p class="card-text">
+                    {{ profile.email }}
+                </p>
+                <ul class="list-unstyled list-inline">
+                    <li><strong>{{profile.Comments.length}}</strong> 已評論餐廳</li>
+                    <li><strong>{{profile.FavoritedRestaurants.length}}</strong> 收藏的餐廳</li>
+                    <li><strong>{{profile.Followings.length}}</strong> followings (追蹤者)</li>
+                    <li><strong>{{profile.Followers.length}}</strong> followers (追隨者)</li>
+                </ul>
+                <p>
+                </p>
+                <form action="" v-if="isAuthenticated">
+                    <button 
+                        type="submit" 
+                        class="btn btn-outline-primary"
+                    >
+                    Edit
+                    </button>
+                </form>
+                <form
+                    v-else
+                    action="/following/3" 
+                    method="POST" 
+                    style="display: contents;"
+                >
+                    <button 
+                        @click.stop.prevent="deleteFollowing" 
+                        v-if="currentIsFollowed"
+                        type="submit" 
+                        class="btn btn-link"
+                    >
+                        已追蹤
+                    </button>
+                    <button 
+                        @click.stop.prevent="addFollowing" 
+                        v-else 
+                        type="submit" 
+                        class="btn btn-primary"
+                    >
+                        追蹤
+                    </button>
+                </form>
+                <p></p>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: '管理者',
+    email: 'root@example.com',
+    image: 'https://i.pravatar.cc/300',
+    isAdmin: true
+  },
+  isAuthenticated: true
+}
+export default {
+    props: {
+        profile: {
+            type: Object,
+            require: true
+        },
+        isFollowed: {
+            type: Boolean,
+            required: true
+        }
+    },
+    data() {
+        return {
+            currentProfile: this.profile,
+            currentIsFollowed: this.isFollowed,
+            isAuthenticated: dummyUser.isAuthenticated
+        }
+    },
+    created(){
+        
+    },
+    methods: {
+        addFollowing() {
+            this.currentIsFollowed = true
+        },
+        deleteFollowing() {
+            this.currentIsFollowed = false
+        },
+
+    }
+}
+</script>
